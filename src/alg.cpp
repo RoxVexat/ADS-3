a5e2751bcce7f1cb01cd496cb6f37726037f62b3
RoxVexat <normalcel976@gmail.com>
2026-04-06 18:43:24 +0300

    Кислинский 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,45 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  uint64_t maxVal = num;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
+    }
+    if (num > maxVal) {
+      maxVal = num;
+    }
+  }
+  return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  unsigned int len = 1;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num = 3 * num + 1;
+    }
+    len++;
+  }
+  return len;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int* maxlen,
+  uint64_t lbound,
+  uint64_t rbound) {
+  unsigned int bestNum = lbound;
+  *maxlen = collatzLen(lbound);
+
+  for (uint64_t i = lbound + 1; i <= rbound; i++) {
+    unsigned int len = collatzLen(i);
+    if (len > *maxlen) {
+      *maxlen = len;
+      bestNum = i;
+    }
+  }
+  return bestNum;
 }
